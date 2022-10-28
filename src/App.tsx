7bb265b228ddd6b00f871591660f8bdb69e0c438
Mohamed-Echbiy <echbiymed@gmail.com>
2022-10-28 22:45:15 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
     <div className="App relative min-h-screen  mx-auto">
-      <div className="navbar_hero_section px-1 sm:px-5 md:px-10 lg:px-20 xl:px-32 h-screen">
+      <div className="navbar_hero_section px-1 sm:px-5 md:px-10 lg:px-20 xl:px-32 min-h-screen">
         <Navbar />
         <HeroSection />
       </div>
